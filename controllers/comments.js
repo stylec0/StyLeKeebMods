f34b72cda90983e4bbeab7c1c31537492c345f8c
Charles Co <stylec0-mac@Charless-MacBook-Air.local>
2022-01-12 21:31:33 -0800

    Working on delete function for comments 9:30pm 1/12/22

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,17 +1,14 @@
 const Keyboard = require('../models/keyboard'); 
 
 module.exports = {
-	create 
-}
+	create,
+    delete: deleteComment,
+};
 
 function create(req, res){
     console.log("Controllers/comments, Create function")
-	
-
 	console.log(req.params.id, " req.params.id")
 	console.log(req.body, " req.body aka the contents of the form")
-
-	
 
 	Keyboard.findById(req.params.id, function(err, keyboardDocument){
         // req.body.userId = req.user._id;
@@ -20,13 +17,40 @@ function create(req, res){
 		keyboardDocument.comments.push(req.body);
 		
 		console.log(keyboardDocument, " <- this is KeyboardDocument, in create comments CTRL")
+        
 		// So have to .save() the document in order to update mongodb
 		keyboardDocument.save(function(err){
 			// redirect the user back to the show page
-
 			// res.redirect(`/keyboards/${keyboardDocument._id}`)
+            console.log(keyboardDocument.comments._id, "<--this is the comments id")
             res.redirect(`/${keyboardDocument._id}/keyboards/details`)
 		})
-	
-	})
+    })
 }
+
+// function deleteComment (req, res) {
+//     console.log("Controller/commments, Delete function")
+//     console.log(req.params.id)
+//     Keyboard.deleteOne(req.params.id);
+//     // Keyboard.findByIdAndRemove(req.params.id, function(err) {
+//         res.redirect(`/${keyboardDocument._id}/keyboards/details`)
+//     }  
+
+function deleteComment(req, res) {
+    // Note the cool "dot" syntax to query on the property of a subdoc
+    Keyboard.findOne(
+      {'comments._id': req.params.id, 'comments.userId': req.user._id},
+      function(err, keyboard) {
+        if (!keyboard || err) return res.redirect(`/keyboards/${keyboard._id}`);
+        // Remove the subdoc (https://mongoosejs.com/docs/subdocs.html)
+        keyboard.comments.remove(req.params.id);
+        // Save the updated book
+        keyboard.save(function(err) {
+          // Redirect back to the book's show view
+          res.redirect(`/keyboards/${keyboard._id}`);
+        });
+      }
+    );
+  }
+
+
--- routes/keyboards.js
+++ routes/keyboards.js
@@ -13,5 +13,6 @@ router.get('/:id/keyboards/new', keyboardsCtrl.new); ///<---create keyboard butt
 router.get('/:id/keyboards/', keyboardsCtrl.index); // <---My keyboards NAV BUtton
 router.post('/:id/keyboards', keyboardsCtrl.create); /// <---add keyboard button in create
 router.post('/:id/keyboards/details', commentsCtrl.create);
+router.delete('/:id/keyboards/details', commentsCtrl.delete);
 
 module.exports = router;
