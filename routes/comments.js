07c7b6e3cddbce4766ee9708da53afa1bbe153f9
Charles Co <stylec0-mac@Charless-MacBook-Air.local>
2022-01-13 11:27:49 -0800

    Deploy to Heroku

--- routes/comments.js
+++ routes/comments.js
@@ -3,19 +3,10 @@ const router = express.Router();
 const commentsCtrl = require('../controllers/comments');
 
 
-// router.post('/keyboards/:id/comments', commentsCtrl.create);
+
 router.post('/:id/keyboards/details', commentsCtrl.create);
 router.delete('/:id/keyboards/:commentsid/details', commentsCtrl.delete);
-// router.delete('/:id/keyboards/details', commentsCtrl.delete);
+
 
  
 module.exports = router;
-
-// const express = require('express');
-// const router = express.Router();
-// const commentsCtrl = require('../controllers/switches');
- 
-// router.get('/:id/keyboards/switches', switchesCtrl.index);
-// router.post('/:id/keyboard/switches', switchesCtrl.create);
- 
-// module.exports = router;
